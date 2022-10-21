ed3572b7b2eb78bd97bb5be6470b32c59b2ae89f
fullstacknaga <fullstacknaga@gmail.com>
2022-10-22 00:16:17 +0530

    Encapsulation

---
+++ src/com/naga/java/interview/oops/encapsulation/Encapsulation.java
@@ -0,0 +1,42 @@
+package com.naga.java.interview.oops.encapsulation;
+
+public class Encapsulation {
+
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private String middleName;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+}
---
+++ src/com/naga/java/interview/oops/encapsulation/EncapsulationDriver.java
@@ -0,0 +1,27 @@
+package com.naga.java.interview.oops.encapsulation;
+
+public class EncapsulationDriver {
+
+	public static void main(String[] args) {
+		
+		Encapsulation encapsulation = new Encapsulation();
+
+		
+		/**
+		 *@Note : below line will gives compilation error because firstName in Encapsulation class is declared as private.
+		 *in this way we will not provide the direct access to class properties. 
+		 *but we ca modify them using setters ans getters.
+		 *
+		 *  
+		 */
+		
+		
+		System.out.println(encapsulation.firstName);
+		
+		//this line won't give any compilation error
+		encapsulation.setFirstName("Naga");
+		
+
+	}
+	
+}
